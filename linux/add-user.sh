377d634968c9bfabfb6b64f0e0dc730454056021
hongquan95 <hongquan95.tran@gmail.com>
2021-08-08 03:19:12 +0700

    update add-user.sh

--- linux/add-user.sh
+++ linux/add-user.sh
@@ -5,7 +5,7 @@ help() {
   Arguments:
   +\$1 given username
   Usage example:
-  $ ./add-docker-user.sh jenkins
+  $ ./add-docker-user.sh tom
 EOF
 }
 
@@ -18,12 +18,14 @@ fi
 
 # generate SSH key pairs
 # REF. https://stackoverflow.com/a/43235320/1235074
-ssh-keygen -q -N '' -m PEM -t rsa -f "$HOME/.ssh/id_rsa_$USR" <<< ""$'\n'"y" 2>&1 >/dev/null
 
+# Run as sudo
+sudo su
 # create new user
 useradd -m -d /home/$USR -s /bin/bash $USR
-usermod -aG docker $USR
 mkdir /home/$USR/.ssh
-touch /home/$USR/.ssh/authorized_keys
-cat "$HOME/.ssh/id_rsa_$USR.pub" > /home/$USR/.ssh/authorized_keys
-ssh -i $HOME/.ssh/id_rsa_$USR $USR@localhost "echo '>>> DONE. New user added'"
+chown dog:dog /home/dog/.ssh/
+su - $USR
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+echo "DONE. User $USR user added"
+exit
